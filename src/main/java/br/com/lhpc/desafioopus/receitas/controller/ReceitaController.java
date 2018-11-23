a01fa55a15bd4cfef49d3b12c847a3c57222f02c
= <=>
2018-11-22 23:32:25 -0200

    TDD

--- src/main/java/br/com/lhpc/desafioopus/receitas/controller/ReceitaController.java
+++ src/main/java/br/com/lhpc/desafioopus/receitas/controller/ReceitaController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import br.com.lhpc.desafioopus.receitas.models.Ingrediente;
 import br.com.lhpc.desafioopus.receitas.models.Receita;
--- src/test/java/br/com/lhpc/desafioopus/receitas/ReceitaControllerTest.java
+++ src/test/java/br/com/lhpc/desafioopus/receitas/ReceitaControllerTest.java
@@ -4,6 +4,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import br.com.lhpc.desafioopus.receitas.models.Ingrediente;
+import br.com.lhpc.desafioopus.receitas.models.Receita;
+import br.com.lhpc.desafioopus.receitas.repository.IngredienteRepository;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
@@ -21,9 +30,32 @@ public class ReceitaControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private IngredienteRepository ingredienteRepository;
+	
+	
+	ObjectMapper mapper = new ObjectMapper();
 
 	@Test
-	public void getReceitasJSON() throws Exception {
+	public void testPostSaveReceitaController() throws Exception {
+		
+		Ingrediente ingrediente = new Ingrediente("Ovo");
+		ingrediente = ingredienteRepository.save(ingrediente);
+
+		Receita receita = new Receita("Ovo frito", 1, 10, Arrays.asList(ingrediente), "frite o ovo na frigideira");
+		
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(receita);
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/receita")
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json)).andExpect(status().isCreated());
+		
+	}
+	
+	@Test
+	public void testGetReceitas() throws Exception {
 
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/receitas"))
 				.andDo(print())
@@ -33,25 +65,55 @@ public class ReceitaControllerTest {
 	}
 	
 	@Test
-	public void getReceitaPorID() throws Exception {
+	public void testGetReceitaPorID() throws Exception {
 		
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/receitas/1"))
-		.andDo(print())
-		.andExpect(status().isOk())
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-		.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
 		
 	}
 	
 	@Test
-	public void getIngredientesPorReceita() throws Exception {
+	public void testGetIngredientesPorReceita() throws Exception {
 		
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/receitas/1/ingrediente"))
-		.andDo(print())
-		.andExpect(status().isOk())
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-		.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
 		
+	}
+	
+	@Test
+	public void testGetReceitaComIngrediente () throws Exception{
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/receitas/-/ingrediente/1"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+		
+	}
+	
+	@Test
+	public void testGetIngredienteEmReceitas () throws Exception {
+		
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/receitas/ingredientes"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+	}
+	
+	@Test
+	public void testGetIngredientes () throws Exception {
+		
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/ingredientes"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
 	}
 
 }
